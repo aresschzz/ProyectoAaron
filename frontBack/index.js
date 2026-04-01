421671bbbc409b119a2972a66f408ccd70d88b56
MauAlcantara1 <japonxd1@hotmail.com>
2026-04-01 16:23:59 -0600

    fix: se arreglaron las referencias de las paginas para su correcta visualizacion

--- frontBack/index.js
+++ frontBack/index.js
@@ -13,27 +13,28 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
 // Cliente
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
-app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
-app.get('/registro', (req, res) => res.sendFile(path.join(__dirname, 'views', 'registro.html')));
-app.get('/catalogo', (req, res) => res.sendFile(path.join(__dirname, 'views', 'catalogo.html')));
-app.get('/detalle', (req, res) => res.sendFile(path.join(__dirname, 'views', 'detalle.html')));
-app.get('/carrito', (req, res) => res.sendFile(path.join(__dirname, 'views', 'carrito.html')));
-app.get('/checkout', (req, res) => res.sendFile(path.join(__dirname, 'views', 'checkout.html')));
-app.get('/confirmacion', (req, res) => res.sendFile(path.join(__dirname, 'views', 'confirmacion.html')));
-app.get('/compras', (req, res) => res.sendFile(path.join(__dirname, 'views', 'compras.html')));
-app.get('/perfil', (req, res) => res.sendFile(path.join(__dirname, 'views', 'perfil.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public','views', 'index.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'login.html')));
+app.get('/registro', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'registro.html')));
+app.get('/catalogo', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'catalogo.html')));
+app.get('/detalle', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'detalle.html')));
+app.get('/carrito', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'carrito.html')));
+app.get('/checkout', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'checkout.html')));
+app.get('/confirmacion', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views',  'confirmacion.html')));
+app.get('/compras', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'compras.html')));
+app.get('/perfil', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'perfil.html')));
 
 
 // Admin
-app.get('/admin/dashboard', (req, res) => res.sendFile(path.join(__dirname, 'views', 'admin', 'dashboard.html')));
-app.get('/admin/inventario', (req, res) => res.sendFile(path.join(__dirname, 'views', 'admin', 'inventario.html')));
-app.get('/admin/agregar-vinilo', (req, res) => res.sendFile(path.join(__dirname, 'views', 'admin', 'agregar-vinilo.html')));
-app.get('/admin/compras-vinilo', (req, res) => res.sendFile(path.join(__dirname, 'views', 'admin', 'compras-vinilo.html')));
-app.get('/admin/nueva-compra', (req, res) => res.sendFile(path.join(__dirname, 'views', 'admin', 'nueva-compra.html')));
-app.get('/admin/ventas', (req, res) => res.sendFile(path.join(__dirname, 'views', 'admin', 'ventas.html')));
-app.get('/admin/usuarios', (req, res) => res.sendFile(path.join(__dirname, 'views', 'admin', 'usuarios.html')));
-app.get('/admin/reportes', (req, res) => res.sendFile(path.join(__dirname, 'views', 'admin', 'reportes.html')));
+// Admin
+app.get('/admin/dashboard', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'admin', 'dashboard.html')));
+app.get('/admin/inventario', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'admin', 'inventario.html')));
+app.get('/admin/agregar-vinilo', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'admin', 'agregar-vinilo.html')));
+app.get('/admin/compras-vinilo', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'admin', 'compras-vinilo.html')));
+app.get('/admin/nueva-compra', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'admin', 'nueva-compra.html')));
+app.get('/admin/ventas', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'admin', 'ventas.html')));
+app.get('/admin/usuarios', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'admin', 'usuarios.html')));
+app.get('/admin/reportes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'views', 'admin', 'reportes.html')));
 
 app.listen(PORT, () => {
   console.log(`VINIL corriendo en http://localhost:${PORT}`);
